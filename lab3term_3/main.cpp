54518c2ead8a86224c8aa6e599c9e32febf1a8ec
Marta Shevchenko <martochka.shevch@gmail.com>
2021-12-06 21:28:58 +0200

    class CellMap and destructor was added

--- lab3term_3/main.cpp
+++ lab3term_3/main.cpp
@@ -1,16 +1,13 @@
-#pragma once
-
-#include <SDL.h>
 #include <iostream>
-#include <windows.h>
+#include <SDL.h>
 
 
-// CellMap stores an array of cells with their states
 class CellMap
 {
 public:
     CellMap(unsigned int w, unsigned int h);
     ~CellMap();
+
 private:
     unsigned char* cells;
     unsigned char* temp_cells;
@@ -19,29 +16,14 @@ private:
     unsigned int length_in_bytes;
 };
 
-// Cell map dimensions
-unsigned int cellmap_width = 500;
-unsigned int cellmap_height = 500;
-
-// Width and height (in pixels) of a cell i.e. magnification
-unsigned int cell_size = 1;
-
-// Randomisation seed
-unsigned int seed;
-
-// Graphics
-SDL_Window *window = NULL;
-SDL_Surface* surface = NULL;
-unsigned int s_width = cellmap_width * cell_size;
-unsigned int s_height = cellmap_height * cell_size;
 
 
-
-int main(int argc, char* argv[])
-{
-    
+int main() {
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+
 
 CellMap::CellMap(unsigned int w, unsigned int h)
 {
@@ -53,11 +35,7 @@ CellMap::CellMap(unsigned int w, unsigned int h)
     memset(cells, 0, length_in_bytes);  // clear all cells, to start
 }
 
-CellMap::~CellMap()
-{
+CellMap::~CellMap() {
     delete[] cells;
     delete[] temp_cells;
 }
-
-
-
